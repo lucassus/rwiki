b10691ce037aee8b29da21173dfca5d01f091afa
Łukasz Bandzarewicz <lucassus@gmail.com>
2010-09-14 11:02:41 +0200

    Better styles for TOC.

--- lib/rwiki/textile_utils.rb
+++ lib/rwiki/textile_utils.rb
@@ -11,7 +11,7 @@ module Rwiki
       
       raw_after_coderay = coderay(raw_content)
       html = parse(raw_after_coderay)
-      toc = "<div class='toc'>" + generate_toc(raw_content) + "</div>\n"
+      toc = "<div class='toc-container'><div class='toc'>" + generate_toc(raw_content) + "</div></div>\n"
 
       return { :raw => raw_content, :html => toc + html }
     end
