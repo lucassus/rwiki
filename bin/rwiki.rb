90f3d7524beb96d11527f2caf3a0744fba38c6ca
Łukasz Bandzarewicz <lucassus@gmail.com>
2010-09-20 15:04:30 +0200

    Better path expanding.

--- bin/rwiki.rb
+++ bin/rwiki.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rwiki'
 
-working_folder = ARGV[0] || Dir.pwd
-unless working_folder.start_with?('/')
-  working_folder = File.join(Dir.pwd, working_folder)
-end
+working_folder = ARGV[0] ? File.expand_path(ARGV[0]) : Dir.pwd
 
 RWIKI_ROOT = working_folder.freeze
 p "Working folder is: #{RWIKI_ROOT}"
