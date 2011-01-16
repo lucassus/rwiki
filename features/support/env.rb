9ce266fb1f8f0c0d001acf2168f1ce547a4686a7
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-01-16 12:34:33 +0100

    TmpdirHelper module.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,9 @@ require 'rspec'
 require 'cucumber/web/tableish'
 require 'capybara'
 require 'capybara/cucumber'
+require File.expand_path(File.join(File.dirname(__FILE__), '../../test/tmpdir_helper'))
+
+Rwiki::App.set(:environment, :test)
 
 Capybara.register_driver :selenium do |app|
   Capybara::Driver::Selenium
@@ -13,33 +16,13 @@ Capybara.register_driver :selenium do |app|
   Capybara::Driver::Selenium.new(app, { :browser => :firefox, :profile => profile })
 end
 
-Rwiki::App.set(:environment, :test)
-
-TMP_DIR = '/tmp/rwiki_test'
-
-def tmp_dir
-  TMP_DIR
-end
-
-def create_tmpdir!
-  FileUtils.mkdir_p(tmp_dir)
-  pages_dir = File.expand_path(File.join(File.dirname(__FILE__), '../../test/fixtures', 'pages'), __FILE__)
-  FileUtils.cp_r(pages_dir, tmp_dir)
-
-  # change working directory to the wiki root
-  @working_path = File.join(tmp_dir, 'pages')
-  Rwiki::Models::Node.working_path = @working_path
-end
-
-def remove_tmpdir!
-  FileUtils.rm_rf(tmp_dir)
-end
-
 World do
   Capybara.app = Rwiki::App
 
   include RSpec::Expectations
   include RSpec::Matchers
+
+  include TmpdirHelper
 
   Before do
     create_tmpdir!
--- test/helper.rb
+++ test/helper.rb
@@ -5,29 +5,12 @@ require 'rubygems'
 require 'test/unit'
 require 'rack/test'
 require 'shoulda'
+require File.expand_path(File.join(File.dirname(__FILE__), 'tmpdir_helper'))
 
 ENV['RACK_ENV'] = 'test'
 
 class Test::Unit::TestCase
-  TMP_DIR = '/tmp/rwiki_test'
-
-  def tmp_dir
-    TMP_DIR
-  end
-
-  def create_tmpdir!
-    FileUtils.mkdir_p(tmp_dir)
-    pages_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', 'pages'), __FILE__)
-    FileUtils.cp_r(pages_dir, tmp_dir)
-
-    # change working directory to the wiki root
-    @working_path = File.join(tmp_dir, 'pages')
-    Rwiki::Models::Node.working_path = @working_path
-  end
-
-  def remove_tmpdir!
-    FileUtils.rm_rf(tmp_dir)
-  end
+  include TmpdirHelper
 
   def setup
     create_tmpdir!
@@ -36,5 +19,4 @@ class Test::Unit::TestCase
   def teardown
     remove_tmpdir!
   end
-
 end
