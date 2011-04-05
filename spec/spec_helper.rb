3481c6f4196f4f45ddeeeabd7d9081488267ff9d
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-04-05 18:04:04 +0200

    Reorganize specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,11 @@ require 'rwiki'
 require 'ap'
 require 'rack/test'
 
-require 'tmpdir_helper'
-require 'rwiki_macros'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.expand_path("support/**/*.rb", File.dirname(__FILE__))].each do |file|
+  require file
+end
 
 Rwiki::App.set :environment, :test
 
@@ -17,7 +20,11 @@ RSpec.configure do |config|
   # == Mock Framework
   config.mock_with :mocha
 
-  config.before(:each) do
+  config.before do
     create_tmpdir!
+  end
+
+  config.after do
+    remove_tmpdir!
   end
 end
