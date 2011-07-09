496a0e6891d2164afb2b5d906da985e6e0225762
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-07-09 19:32:43 +0200

    Better flash messages.

--- public/javascripts/Rwiki/Flash.js
+++ public/javascripts/Rwiki/Flash.js
@@ -1,31 +1,43 @@
 Ext.ns('Rwiki');
 
 Rwiki.Flash = function() {
+  this._container = null;
   this._time = 2;
 };
 
 Rwiki.Flash.prototype = {
   success: function(message) {
-    this._flash(message, 'success');
+    this._flash(message, 'Success');
   },
 
   error: function(message) {
-    this._flash(message, 'error');
+    this._flash(message, 'Error');
   },
 
   warning: function(message) {
-    this._flash(message, 'warning');
+    this._flash(message, 'Warning');
   },
 
   info: function(message) {
-    this._flash(message, 'info');
+    this._flash(message, 'Info');
   },
 
-  _flash: function(message, type) {
-    Ext.ux.Msg.flash({
-      msg: message,
-      time: this._time,
-      type: type
-    });
+  _createBox: function(title, message) {
+    return ['<div class="msg">',
+      '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
+      '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc"><h3>', title, '</h3>', message, '</div></div></div>',
+      '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>',
+      '</div>'].join('');
+  },
+
+  _flash: function(message, title) {
+    if (!this._container) {
+      this._container = Ext.DomHelper.insertFirst(document.body, { id:'msg-div' }, true);
+    }
+
+    this._container.alignTo(document, 't-t');
+    var m = Ext.DomHelper.append(this._container, { html: this._createBox(title, message) }, true);
+    m.slideIn('t').pause(1).ghost("t", { remove: true });
   }
 };
+
